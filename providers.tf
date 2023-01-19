a021a43b5987e9b21450e8b794a5b7475a548bad
Manikandan Ravichandran <mravich1@eu.trimblecorp.net>
2023-01-19 17:37:59 +0530

    terraform

--- providers.tf
+++ providers.tf
@@ -1,6 +1,3 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = "ASIAWUH6QJOWP3WER4U2"
-  secret_key = "XwwqUYViLM83nYYUgDW9/dVf6SPMhGzyPOA1qNn/"
-  aws_session_token = "FwoGZXIvYXdzEBwaDBKg8UsLI3ZtCFezNyL6Ae0T1ahWpRKWyXlWLkuK6TKRCzk68sFKzHqty+6spqqtiRkUaHvFS8+VwqKTMkjcIqqw7si7zpJ5yr77yMDYveK/BN7516hdS2Z2W1Ytg27CVx4+RDY/Ik2zVTYsuUOEeTWC2y+X7TMA9uNDvGx6/l16tT4cyECma2Na65YsjlpU4SHsApqH2VquFIaUe6MEklCZxe23zkzkzAszvXEwDx+lG5T4Hu7HxGteGr2N6PytR175K2BEh0JxgKRiD+QZvrY1xGrt0Otjp6u+cZHRmUCzVbmKYBjl5LhIQ6MTGNFIaQR6UDWlgHW0RvSfIjXVGpMpsBI5H8Bcd5UojsakngYyKze8YchhSU1+L/uie1yQXmKT2XFG/nEvM6sBZeTzh+k5Wv+Rk2TvCO2l+VU="
 }
