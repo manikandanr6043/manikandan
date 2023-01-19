de1425bba572374c3d414a4e5d593b1cf17b5ebe
manikandanr6043 <42379581+manikandanr6043@users.noreply.github.com>
2023-01-19 21:57:12 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_tls1" {
-  name        = "allow_tls1"
+  name        = "allow_tls2"
   description = "Allow TLS inbound traffic"
   vpc_id      = "vpc-0350118ca58f04635"
 }
