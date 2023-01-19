8d4bb22f08006e783bf66b8198e8946c0f1381af
manikandanr6043 <42379581+manikandanr6043@users.noreply.github.com>
2023-01-19 18:55:21 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,3 @@
 provider "aws" {
   region = "us-west-2"
-  aws_access_key = "ASIAWUH6QJOWIEML4IOE"
-  aws_secert_access_key = "/J7S8t8sSKTrx29zuzkxzpgNXMHef22zSF1mAu0D"
-  aws_session_token = "FwoGZXIvYXdzEB8aDAsgtTDGVAVftdbb4SL6AZx26T7c785QROqrlUN6rQw1XkhpWvNgkkSs0dKSzs3dSQ21bzRiqC4+yISO5TyMQ1R+1iZPS/BSm5AMdMMbu8u5pFC31S0qASG/SZrGSZeiF/PNhUyCccdo6EZrr5AEkYojAOJTX2xYF4TvMOF7ZlGQP8qgMpnLdvck7vnYF1epGYfcCW/As/C5u3VqcCzUDKioXRq9MGOYQCJlubP/ksaYWK5EfOfAdV9U/m28dUK1dfepc5DRbXvLQBsIPFPkTOYgNsTd+/iFRuwh9Cb8aX9B92hucArmfteS04f+kZjbKv6nJcDe4J1rwubKG3usDd5eEaq59FqGchcooYGlngYyK5rKFS5uiW9hEntGs/ZGW8nB6Ys/RZsyVfyk5170nOct1urQTACu2/YC6Ko="
 }
